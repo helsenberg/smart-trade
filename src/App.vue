083a9257b34bd9af8cb556e3ad68bb1173551674
helsenberg <antonovdiman71@gmail.com>
2022-01-11 17:23:06 +0300

    Created a first structure of an project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <Header />
+      <v-main>
+        <General />
+      </v-main>
+    <Footer />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header';
+import General from './components/General';
+import Footer from './components/Footer';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Header,
+    General,
+    Footer
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <div class="b-marquee">
+  <div class="b-marquee__text running">I'm just testing a running line</div>
+  </div>
+  <div class="b-marquee b-marquee--rtl">
+    <div class="b-marquee__text">Working in progress</div>
+  </div>
+  </div>
+</template>
+
+<script>
+
+export default ({
+  data() {
+    
+  },
+})
+</script>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+.runningLine {
+  
+}
+
+.b-marquee {
+  font-family: 'Segoe UI', sans-serif;
+  white-space: nowrap;
+  overflow: hidden;
+  color: rgb(0, 0, 0);
+  padding: 10px;
+  position: relative;
+  margin-bottom: 10px;
+}
+
+.b-marquee__text {
+  -webkit-animation: animMarquee 15s linear infinite;
+  animation: animMarquee 15s linear infinite;
+}
+
+@-webkit-keyframes animMarquee {
+  0% {
+    -webkit-transform: translateX(0);
+    transform: translateX(0);
+  }
+  100% {
+    -webkit-transform: translateX(100%);
+    transform: translateX(100%);
+  }
+}
+
+@keyframes animMarquee {
+  0% {
+    -webkit-transform: translateX(0);
+    transform: translateX(0);
+  }
+  100% {
+    -webkit-transform: translateX(100%);
+    transform: translateX(100%);
+  }
+}
+
+
+/* text right to left*/
+
+.b-marquee--rtl {}
+
+.b-marquee--rtl .b-marquee__text {
+  -webkit-animation: animMarqueeRtl 10s linear infinite;
+  animation: animMarqueeRtl 10s linear infinite;
+}
+
+@-webkit-keyframes animMarqueeRtl {
+  0% {
+    -webkit-transform: translateX(100%);
+    transform: translateX(100%);
+  }
+  100% {
+    -webkit-transform: translateX(0%);
+    transform: translateX(0%);
+  }
+}
+
+@keyframes animMarqueeRtl {
+  0% {
+    -webkit-transform: translateX(100%);
+    transform: translateX(100%);
+  }
+  100% {
+    -webkit-transform: translateX(0);
+    transform: translateX(0);
+  }
+}
+</style>
